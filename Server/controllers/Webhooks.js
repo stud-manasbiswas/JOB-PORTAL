604e4591ba73732cacd25718979a48dbe37a02da
stud-manasbiswas <manasbiswas342@gmail.com>
2025-07-17 11:57:23 +0530

    CHECK

--- Server/controllers/Webhooks.js
+++ Server/controllers/Webhooks.js
@@ -1,70 +1,54 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
-// API Controller Function To Manage Clerk user with database
-const ClerkWebhooks = async (req,res)=>{
-    try{
-        // Create a svix instance with clerk webhook secret
-        const whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET_RENDER)
+const ClerkWebhooks = async (req, res) => {
+  try {
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        // Verifying Headers
+    const evt = wh.verify(req.body, {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"]
+    });
 
-      await whook.verify(req.body, {
-  "svix-id": req.headers["svix-id"],
-  "svix-timestamp": req.headers["svix-timestamp"],
-  "svix-signature": req.headers["svix-signature"]
-});
-        // Getting Data from request body
+    const { data, type } = evt;
 
-        const {data,type} =req.body
+    console.log("🔔 Webhook received:", type);
 
-        // Switch cases for different events
-
-        switch (type) {
-            case 'user.created':{
-
-                const userData = {
-                    _id:data.id,
-                    email:data.email_addresses[0].email_addresses,
-                    name: data.first_name + " " + data.last_name,
-                    image: data.image_url,
-                    resume : ' '
-                }
-                await User.create(userData)
-                res.json({})
-                break;
-
-            }
-            case 'user.updated':{
-                    const userData = {
-                    
-                    email:data.email_addresses[0].email_addresses,
-                    name: data.first_name + " " + data.last_name,
-                    image: data.image_url,
-                    
-                }
-                await User.findByIdAndUpdate(data.id,userData)
-                res.json({})
-                break;
-
-            }
-            case 'user.deleted':{
-                await User.findByIdAndDelete(data.id)
-                res.json({})
-                break;
-
-
-
-            }
-            default: break;
-                
-              
-        }
+    switch (type) {
+      case 'user.created': {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          image: data.image_url,
+          resume: ' '
+        };
+        await User.create(userData);
+        break;
+      }
+      case 'user.updated': {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          image: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        break;
+      }
+      case 'user.deleted': {
+        await User.findByIdAndDelete(data.id);
+        break;
+      }
+      default:
+        break;
     }
-    catch(error){
-        console.log(error.message);
-        res.json({success:false,message:'Webhooks error'})
-    }
-}
 
-export default ClerkWebhooks
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("❌ Webhook error:", error.message);
+    res.status(400).json({ success: false, message: 'Webhook failed' });
+  }
+};
+
+export default ClerkWebhooks;
--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDb from './config/db.js'
 import * as Sentry from "@sentry/node";
-import clerkWebhooks from './controllers/Webhooks.js'
+import ClerkWebhooks from './controllers/Webhooks.js'
 
 
 // initialize express
@@ -24,7 +24,7 @@ app.get('/',(req,res)=> res.send('API Working'))
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
-app.post('/webhooks',clerkWebhooks)
+app.post('/webhooks',ClerkWebhooks)
 
 //port
  const PORT = process.env.PORT || 5000
